55d2e34f89253de90444046844c00804099e79a4
Steven <thevgm01@gmail.com>
2020-09-03 17:31:45 -0500

    Added basic character controller and third person camera

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ThirdPersonMovement : MonoBehaviour
+{
+    CharacterController _controller;
+    public Transform _camera;
+
+    public float speed = 6f;
+
+    public float turnSmooth = 0.1f;
+    private float turnSmoothVelocity;
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if(direction.sqrMagnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmooth);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+}
