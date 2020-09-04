c0479874fb1775be23d53dd9d32621bc76381ed4
Steven <thevgm01@gmail.com>
2020-09-04 16:34:07 -0500

    Added a character model and basic animations

---
+++ Assets/PlayerCharacterAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCharacterAnimator : MonoBehaviour
+{
+    [SerializeField] ThirdPersonMovement _thirdPersonMovement = null;
+
+    const string IdleState = "Idle";
+    const string RunState = "Running";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+
+    Animator _animator = null;
+    [SerializeField] float crossFadeTime = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, crossFadeTime);
+    }
+
+    private void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, crossFadeTime);
+    }
+
+    void OnEnable()
+    {
+        _thirdPersonMovement.Idle += OnIdle;
+        _thirdPersonMovement.StartRunning += OnStartRunning;
+    }
+    
+    void OnDisable()
+    {
+        _thirdPersonMovement.Idle -= OnIdle;
+        _thirdPersonMovement.StartRunning -= OnStartRunning;
+    }
+}
--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+
+    bool _isMoving = false;
+
     CharacterController _controller;
     public Transform _camera;
 
@@ -18,6 +24,11 @@ public class ThirdPersonMovement : MonoBehaviour
         _controller = GetComponent<CharacterController>();
     }
 
+    void Start()
+    {
+        Idle?.Invoke();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -27,12 +38,35 @@ public class ThirdPersonMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if(direction.sqrMagnitude >= 0.1f)
         {
+            SetMoving(true);
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmooth);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             _controller.Move(move * speed * Time.deltaTime);
+        }
+        else
+        {
+            SetMoving(false);
+        }
+
+        float jump = Input.GetAxisRaw("Jump");
+
+    }
+
+    void SetMoving(bool move)
+    {
+        if(move && !_isMoving)
+        {
+            StartRunning?.Invoke();
+            _isMoving = true;
+        }
+        else if(!move && _isMoving)
+        {
+            Idle?.Invoke();
+            _isMoving = false;
         }
     }
 }
